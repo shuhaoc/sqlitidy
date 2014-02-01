075f48120d0ab8ea24b0e71d3418f5fb4ba10446
shuhaoc <shuhaoc@qq.com>
2014-02-01 23:36:50 +0800

    完成基本功能

---
+++ src/sqlitidy/main.cpp
@@ -0,0 +1,74 @@
+// sqlitidy.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "sqlitidy.h"
+
+using namespace std;
+using namespace sqlitidy;
+
+struct User {
+	int id;
+	string name;
+	int age;
+
+	static const string tableName;
+
+	static const string keyName;
+
+	static const bool pkAutoInc = true;
+
+	static const array<string, 3> fieldNames;
+
+	SQLITIDY_VALUE_GETTER_BEGIN
+		SQLITIDY_VALUE_GETTER(id)
+		SQLITIDY_VALUE_GETTER(name)
+		SQLITIDY_VALUE_GETTER(age)
+	SQLITIDY_VALUE_GETTER_END
+
+	SQLITIDY_VALUE_SETTER_BEGIN
+		SQLITIDY_INT_VALUE_SETTER(id)
+		SQLITIDY_STRING_VALUE_SETTER(name)
+		SQLITIDY_INT_VALUE_SETTER(age)
+	SQLITIDY_VALUE_SETTER_END
+
+	User() : id(-1), age(-1) { }
+
+private:
+	User(const User&);
+	User& operator = (const User&);
+};
+
+const string User::tableName = "tb_user";
+
+const string User::keyName = "id";
+
+const array<string, 3> User::fieldNames = { "id", "name", "age" };
+
+template <typename Container> void deleteContainer(const Container& container) {
+	for (auto i = container.begin(); i != container.end(); i++) {
+		delete *i;
+	}
+}
+
+int _tmain(int argc, _TCHAR* argv[]) {
+	DbContext ctx("test.db");
+	//User u;
+	//cin >> u.id >> u.name >> u.age;
+	//ctx.save(u);
+	//cout << u.id << u.name << u.age << endl;
+	//User u2;
+	//ctx.load(u.id, u2);
+	//cout << u2.id << u2.name << u2.age << endl;
+	array<DbValue, 1> p1 = { 14 };
+	vector<User*> ul;
+	//ctx.where("id > ?", p1, ul);
+	//ctx.where("id > 10", ul);
+	ctx.all(ul);
+	for_each(ul.begin(), ul.end(), [] (User* u) {
+		cout << u->id << u->name << u->age << endl;
+	});
+	deleteContainer(ul);
+	return 0;
+}
+
---
+++ src/sqlitidy/sqlitidy.cpp
@@ -0,0 +1,76 @@
+#include "sqlitidy.h"
+
+namespace sqlitidy {
+
+void DbValue::assign(const DbValue& value) {
+	assert(type == value.type);
+	switch (type) {
+	case SQLITE_INTEGER:
+		intValue = value.intValue;
+		break;
+	case SQLITE_TEXT: {
+		delete[] stringValue;
+		unsigned len = strlen(value.stringValue) + 1;
+		stringValue = new char[len];
+		strcpy(stringValue, value.stringValue);
+	}
+	break;
+	default:
+		assert(false && "Invalid value type");
+	}
+}
+
+DbContext::DbContext(const std::string& filePath) {
+	int ec = ::sqlite3_open(filePath.c_str(), &db);
+	assert(ec == SQLITE_OK);
+}
+
+DbContext::~DbContext() {
+	::sqlite3_close(db);
+}
+
+void DbContext::bind(sqlite3_stmt* stmt, int i, const DbValue& value) {
+	int ec = SQLITE_OK;
+	switch (value.type) {
+	case SQLITE_INTEGER:
+		ec = ::sqlite3_bind_int(stmt, i, value.intValue);
+		break;
+	case SQLITE_TEXT:
+		ec = ::sqlite3_bind_text(stmt, i, value.stringValue, strlen(value.stringValue), SQLITE_TRANSIENT);
+		break;
+	default:
+		assert(false && "Invalid value type");
+	}
+	assert(ec == SQLITE_OK);
+}
+
+sqlite3_stmt* DbContext::compile(const std::string& sql) {
+	sqlite3_stmt* stmt = nullptr;
+	const char* tail = nullptr;
+	int ec = ::sqlite3_prepare(db, sql.c_str(), sql.size(), &stmt, &tail);
+	assert(ec == SQLITE_OK);
+	return stmt;
+}
+
+int DbContext::step(sqlite3_stmt* stmt) {
+	assert(stmt);
+	int ec = ::sqlite3_step(stmt);
+	assert(ec == SQLITE_DONE || ec == SQLITE_ROW);
+	return ec;
+}
+
+DbValue DbContext::extract(sqlite3_stmt* stmt, int i) {
+	assert(stmt);
+	int type = ::sqlite3_column_type(stmt, i);
+	switch (type) {
+	case SQLITE_INTEGER:
+		return ::sqlite3_column_int(stmt, i);
+	case SQLITE_TEXT:
+		return reinterpret_cast<const char*>(::sqlite3_column_text(stmt, i));
+	default:
+		assert(false && "Invalid value type");
+		return DbValue();
+	}
+}
+
+} // namespace sqlitidy
